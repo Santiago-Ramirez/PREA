8d0b4cb38580adb9f9f5f25d92ca4cd7fbe2a84c
ubaldo de santiago <ubaldo_desantiago10@outlook.com>
2022-05-25 18:39:34 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+<title>CV - PREA</title>
+</head>
+<body>
+	
+
+
+  
+<?php
+$conn=new PDO('mysql:host=localhost; dbname=uploadfile', 'root', '') or die(mysql_error());
+
+if(isset($_POST['submit'])!=""){
+
+  $name=$_FILES['file']['name'];
+  $size=$_FILES['file']['size'];
+  $type=$_FILES['file']['type'];
+  $temp=$_FILES['file']['tmp_name'];
+  // $caption1=$_POST['caption'];
+  // $link=$_POST['link'];
+  $fname = date("YmdHis").'_'.$name;
+  $chk = $conn->query("SELECT * FROM  upload where name = '$name' ")->rowCount();
+  
+  if($chk){
+    $i = 1;
+    $c = 0;
+	while($c == 0){
+    	$i++;
+    	$reversedParts = explode('.', strrev($name), 2);
+    	$tname = (strrev($reversedParts[1]))."_".($i).'.'.(strrev($reversedParts[0]));
+    // var_dump($tname);exit;
+    	$chk2 = $conn->query("SELECT * FROM  upload where name = '$tname' ")->rowCount();
+		
+    	if($chk2 == 0){
+    		$c = 1;
+    		$name = $tname;
+    	}
+    }
+}
+ $move =  move_uploaded_file($temp,"upload/".$fname);
+ if($move){
+ 	$query=$conn->query("insert into upload(name,fname)values('$name','$fname')");
+	
+	if($query){
+		
+	header("location:index.php");
+	}
+	else{
+	die(mysql_error());
+	}
+ }
+}
+
+?>
+<html>
+<head>
+<title>Subir y Descargar Archivos en PHP y MYSQL</title>
+		<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="screen">
+        <link rel="stylesheet" type="text/css" href="css/DT_bootstrap.css">
+</head>
+	<script src="js/jquery.js" type="text/javascript"></script>
+	<script src="js/bootstrap.js" type="text/javascript"></script>
+	
+	<script type="text/javascript" charset="utf-8" language="javascript" src="js/jquery.dataTables.js"></script>
+	<script type="text/javascript" charset="utf-8" language="javascript" src="js/DT_bootstrap.js"></script>
+
+<style>
+</style>
+<body>
+	    <div class="row-fluid">
+	        <div class="span12">
+	            <div class="container">
+		<br />
+<center>		<img src="LOGO.PNG" alt="corporativoPreaLogo" width="500px" height="500px" ><br><br><br><br>
+</center>
+		<br />
+		<br />
+			<form enctype="multipart/form-data" action="" name="form" method="post">
+				Suba su CV aquí por favor
+					<input type="file" name="file" id="file" /><br></td>
+					<input type="submit" name="submit" id="submit" value="Subir" class="btn btn-success"/>
+			</form>
+			
+			<p style="font-size: 10px;">La información que sea enviada esta cifrada de extremo a extremo. nadie fuera de esta plataforma, puede ver o descargar la información.</p>
+		
+	</div>
+	</div>
+	</div>
+
+	 
+<style>html {
+  min-height: 100%;
+  position: relative;
+}
+body {
+  margin: 0;
+  margin-bottom: 40px;
+}
+footer {
+  background-color: #145DF8;
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  height: 40px;
+  color: white;
+}</style>
+
+	<footer>
+<center>  © COPYRIGHT SISTEMAS CORPORATIVO PREA@2022
+</center></footer>
+
+</body>
+</html>
